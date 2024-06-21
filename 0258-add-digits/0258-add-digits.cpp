3a6149d0f3e88e292628458e4604e421b636c419
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2024-06-21 14:02:24 +0530

    Time: 0 ms (100%), Space: 7.5 MB (57.17%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        if(num%9==0 && num!=0){
+            return 9;
+        }
+        else if(num>9){
+            return num%9;
+        }
+        return num;
+
+
+
+        
+
+        
+    }
+};

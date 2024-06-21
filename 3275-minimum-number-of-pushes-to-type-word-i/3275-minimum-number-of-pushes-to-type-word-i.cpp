d7933c7a16883891155598cec4582a5917b89600
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2024-06-21 14:48:43 +0530

    Time: 4 ms (33.33%), Space: 7.5 MB (65.82%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,11 +4,6 @@ public:
         int n = word.size();
         int r = n%8;
         int q = n/8;
-        // cout<<n;
-        // if(n%8==0) return 8*((q*(q+1))/2);
-        // if(n<16){
-        //     return r*(q+1) + q*8;
-        // }
         
         return r*(q+1) + 8*((q*(q+1))/2);
         

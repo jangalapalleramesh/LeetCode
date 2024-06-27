8b6c5d73462c8439ae1b7d2ef50fe25bc07663dd
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2024-06-27 22:38:31 +0530

    Time: 2 ms (83.81%), Space: 10.4 MB (37.59%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        // Remove leading and trailing spaces
+        trim(s);
+        
+        // Split the string into words
+        vector<string> words = split(s);
+        
+        // Reverse the words
+        reverse(words.begin(), words.end());
+        
+        // Join the words back into a single string with a single space between each word
+        string result = join(words);
+        
+        return result;
+        
+    }
+
+private:
+    void trim(string &s){
+        int start = s.find_first_not_of(' ');
+        int end = s.find_last_not_of(' ');
+        s = (start == string::npos)?"":s.substr(start,end-start+1);
+    }
+
+    // spilt function string into word
+
+    vector<string> split(const string &s){
+        vector<string> words;
+        istringstream iss(s);
+        string word;
+        while(iss>> word){
+            words.push_back(word);
+        }
+
+        return words;
+
+    }
+
+      // Function to join words into a single string
+    string join(const vector<string> &words) {
+        string result;
+        for (const string &word : words) {
+            if (!result.empty()) {
+                result += " ";
+            }
+            result += word;
+        }
+        return result;
+    }
+};

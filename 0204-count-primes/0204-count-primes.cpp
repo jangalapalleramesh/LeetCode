1839221fc9707fd57b502c4ad7cf15e837549de9
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2024-10-11 22:41:31 +0530

    Time: 120 ms (93.01%), Space: 131.9 MB (15.82%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -10,7 +10,7 @@ public:
     
         for(int i=2;i*i<=n;i++){
             if(primes[i]){
-                for(int j = i*i;j<n;j+=i){
+                for(int j = i*i;j<=n;j+=i){
                     primes[j]=0;
                 }
             }

2103b45d40477c8af76768709c2fbe8b14ce7884
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2024-12-26 14:29:01 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.88%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -20,7 +20,7 @@ class Solution {
         long nn = n;
         if(nn<0) nn = -1*nn;
         while(nn!=0){
-            if(nn%2==0){
+            if((nn&1)==0){
                 x=x*x;
                 nn=nn>>1;
             }

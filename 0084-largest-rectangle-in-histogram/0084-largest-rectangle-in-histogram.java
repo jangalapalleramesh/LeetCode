9085fefccedf52f7bec72806192bc7f9df3f687d
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2025-01-02 15:42:10 +0530

    Time: 65 ms (63.69%), Space: 57.9 MB (25.66%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,81 +1,22 @@
-class Pair {
-    int val;
-    int index;
-
-    Pair(int val, int index) {
-        this.val = val;
-        this.index = index;
-    }
-}
-
 class Solution {
-    private int[] nextSmallerElement(int[] heights) {
-        int n = heights.length;
-        int[] nse = new int[n];
-        Stack<Pair> st = new Stack<>();
-        nse[n - 1] = n;
-        st.push(new Pair(heights[n - 1], n - 1));
-        for (int i = n - 2; i >= 0; i--) {
-            while (!st.isEmpty() && heights[i] <= st.peek().val) {
-                st.pop();
-            }
-            nse[i] = (st.isEmpty()) ? n : st.peek().index;
-            st.push(new Pair(heights[i], i));
-        }
-
-        return nse;
-    }
-
-    private int[] prevSmallerElement(int[] heights) {
-        int n = heights.length;
-        int[] pse = new int[n];
-        Stack<Pair> st = new Stack<>();
-        pse[0] = -1;
-        st.push(new Pair(heights[0], 0));
-        for (int i = 1; i < n; i++) {
-            while (!st.isEmpty() && heights[i] <= st.peek().val) {
-                st.pop();
-            }
-            pse[i] = (st.isEmpty()) ? -1 : st.peek().index;
-            st.push(new Pair(heights[i], i));
-        }
-        return pse;
-    }
-
     public int largestRectangleArea(int[] heights) {
+        //Take one stack data Structure
         int n = heights.length;
-        //int[] nse = nextSmallerElement(heights);
-        // int[] pse = prevSmallerElement(heights);
+        Stack<Integer> st = new Stack<Integer>();
         int maxArea = 0;
-        Stack<Pair> st = new Stack<>();
-        st.push(new Pair(heights[0],0));
-        for (int i = 1; i < n; i++) {
-            while(!st.isEmpty()&& i<n &&st.peek().val<=heights[i]){
-                st.push(new Pair(heights[i],i));
-                i++;
-            }
-           
-            
-            while(!st.isEmpty()&& i<n&& st.peek().val>heights[i]){
-                int cal = st.peek().val;
-               
-                st.pop();
-                
+        for(int i=0;i<heights.length;i++){
+            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
+                int cal = heights[st.pop()];
+                maxArea = (st.isEmpty())?Math.max(maxArea,cal*i):Math.max(maxArea,cal*(i-st.peek()-1));
 
-                maxArea = (st.isEmpty())?Math.max(maxArea,cal*(i-(-1)-1)):Math.max(maxArea,cal*(i-st.peek().index-1));
-                
             }
-            if(i<n)st.push(new Pair(heights[i],i));
-
-            
+            st.push(i);
         }
         while(!st.isEmpty()){
-            int cal = st.peek().val;
-            st.pop();
-            
-            maxArea = (st.isEmpty())?Math.max(maxArea,cal*(n-(-1)-1)):Math.max(maxArea,cal*(n-st.peek().index-1));
+            int cal = heights[st.pop()];
+             maxArea = (st.isEmpty())?Math.max(maxArea,cal*n):Math.max(maxArea,cal*(n-st.peek()-1));
         }
         return maxArea;
-
+        
     }
 }

62f8494a3bdcc73f32817f79567fb127fbf2c33d
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2025-01-06 17:13:58 +0530

    Time: 177 ms (6.6%), Space: 45.4 MB (5.03%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.java
@@ -3,12 +3,26 @@ class Solution {
         int n = boxes.length();
         int ans[] = new int[n];
         int count = 0;
+        // for(int i=0;i<n;i++){
+        //     count = 0;
+        //     for(int j=0;j<n;j++){
+        //         if(boxes.charAt(j)=='1') count += Math.abs(j-i);
+        //     }
+        //     ans[i] = count ;
+
+        // }
+        List<Integer> indexOfOnes = new ArrayList<Integer>();
+        for(int i=0;i<n;i++){
+            if(boxes.charAt(i)=='1'){
+                indexOfOnes.add(i);
+            }
+        }
         for(int i=0;i<n;i++){
             count = 0;
-            for(int j=0;j<n;j++){
-                if(boxes.charAt(j)=='1') count += Math.abs(j-i);
+            for(int num:indexOfOnes){
+                count += Math.abs(num-i);
             }
-            ans[i] = count ;
+            ans[i] = count;
 
         }
         return ans;

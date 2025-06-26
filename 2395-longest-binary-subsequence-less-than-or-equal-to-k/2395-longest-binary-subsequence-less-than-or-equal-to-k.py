a91fcc154eb8c1adc89247d44500753f3ffb1d52
jangalapalleramesh <113569073+jangalapalleramesh@users.noreply.github.com>
2025-06-26 08:16:40 +0530

    Time: 0 ms (100%), Space: 17.7 MB (86.99%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.py
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -1,25 +1,15 @@
 class Solution:
     def longestSubsequence(self,s: str, k: int) -> int:
-        count = 0         # Count of characters included in subsequence
-        value = 0         # Decimal value of current subsequence
-        power = 1         # Binary place value (1, 2, 4, 8,...)
-
-        # Traverse from right to left (least significant bit to most)
+        zeros = s.count('0')
+        count = zeros
+        value = 0
+        power = 1
         for ch in reversed(s):
-            if ch == '0':
-                count += 1  # Always safe to include 0
-            else:
+            if ch == '1':
                 if power <= k and value + power <= k:
                     value += power
                     count += 1
-                # Else we skip this '1'
-
             power *= 2
-            if power > k:  # No point in increasing power beyond k
+            if power > k:
                 break
-
-        # Count all remaining 0s in the part we didn't process
-        # Because we may have skipped some 0s at the start
-        count += s[:len(s) - (power.bit_length() - 1)].count('0')
-
         return count
